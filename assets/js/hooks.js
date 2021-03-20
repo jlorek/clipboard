97516755784b29a44ad0c78c239cdf1cd9b17107
jlorek <lorek.jens@gmail.com>
2021-03-20 15:01:14 +0100

    basic alpine examples

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,15 +1,30 @@
 export default {
     Counter: {
         mounted() {
-            console.log("Counter mounted")
+            console.log("counter mounted")
             window.counterHook = this
+            // this.el.addEventListener("count-updated", e => {
+            //     console.log("got count-updated event", e)
+            // })
+        },
+        updated() {
+            console.log("counter updated");
+            var count = this.el.getAttribute("count");
+            console.log("count attribute", count)
+            var event = new CustomEvent("count-updated", {
+                detail: {
+                    count: count
+                }
+            });
+            console.log("dispatching event", event);
+            this.el.dispatchEvent(event);
         },
         decrement() {
-            console.log("Hook decrement")
+            console.log("hook function decrement")
             this.pushEvent("decrement", {})
         },
         increment(selector) {
-            console.log("Hook increment")
+            console.log("hook function increment")
             this.pushEventTo(selector, "increment", {})
         }
     }
--- lib/clipboard_web/live/counter_live.ex
+++ lib/clipboard_web/live/counter_live.ex
@@ -15,14 +15,21 @@ defmodule ClipboardWeb.CounterLive do
 
   def render(assigns) do
     ~L"""
-    <div id="counter" phx-hook="Counter" x-data="{}">
-      counter_live.ex
-      <h1>The counter is: <span><%= @count %></span></h1>
+    <div id="counter"
+         phx-hook="Counter"
+         count=<%= @count %>
+          x-data="{count: 0}"
+          x-on:count-updated="count = $event.detail.count">
+      <h1>The assigns counter is: <span><%= @count %></span></h1>
+      <h1>The alpine count is: <span x-text="count"></span></h1>
       <button phx-click="decrement">Decrement</button>
       <button phx-click="increment">Increment</button>
-
-      <button @click="counterHook.decrement()">Alpine Decrement</button>
-      <button @click="counterHook.increment('#counter')">Alpine Increment</button>
+      <br />
+      <button @click="counterHook.decrement()">Alpine hook decrement</button>
+      <button @click="counterHook.increment('#counter')">Alpine hook increment</button>
+      <br />
+      <button @click="counterHook.pushEvent('decrement', {})">Alpine direct decrement</button>
+      <button @click="counterHook.pushEventTo('#counter', 'increment', {})">Alpine direct increment</button>
     </div>
     """
   end
