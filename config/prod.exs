5c4ae17de0ec9c9628c4b1dde4360efb2d38fd87
jlorek <lorek.jens@gmail.com>
2021-03-26 21:19:42 +0100

    ?

--- config/prod.exs
+++ config/prod.exs
@@ -62,7 +62,7 @@ config :clipboard, ClipboardWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true,
-  check_origin: false,
+  check_origin: false
 
 config :clipboard, Clipboard.Repo,
   adapter: Ecto.Adapters.Postgres,
