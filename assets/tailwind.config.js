ae58b650f1afc2be627f3596df00418f6e43f11a
jlorek <lorek.jens@gmail.com>
2021-04-05 17:37:32 +0200

    coolors

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -11,7 +11,38 @@ module.exports = {
       sans: ['Nunito'],
       serif: ['Open Sans']
     },
-    extend: {},
+    extend: {
+      /* THANKS! */
+      /* https://coolors.co/89a3ca-453197-f1e7e4-d1829f-e99b8a*/
+      colors: {
+        one: {
+          light: '#89A3CA',
+          DEFAULT: '#6A8BBC'
+        },
+        two: {
+          light: '#453197',
+          DEFAULT: '#2A1E5C'
+        },
+        three: {
+          light: '#F1E7E4',
+          DEFAULT: '#EEE2DF'
+        },
+        four: {
+          light: '#D1829F',
+          DEFAULT: '#C66488'
+        },
+        five: {
+          light: '#E99B8A',
+          DEFAULT: '#E4846E'
+        },
+        'font-dark': {
+          DEFAULT: '#011638'
+        },
+        'font-light': {
+          DEFAULT: '#F1FFFA'
+        }
+      },
+    },
   },
   variants: {
     extend: {
