249de0a7a8b6c3b326d54310d6abaaa1b843e91e
jlorek <lorek.jens@gmail.com>
2021-04-07 21:11:23 +0200

    logo

--- lib/clipboard_web/live/welcome_live.ex
+++ lib/clipboard_web/live/welcome_live.ex
@@ -2,15 +2,25 @@ defmodule ClipboardWeb.WelcomeLive do
   use ClipboardWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = socket |> assign(:slogan, Clipboard.SloganGenerator.generate_slogan)
+    slogan =
+      case connected?(socket) do
+        false -> "..."
+        true -> Clipboard.SloganGenerator.generate_slogan()
+      end
+
+    socket = socket |> assign(:slogan, slogan)
     {:ok, socket}
   end
 
   def handle_event("new_topic", _params, socket) do
-    socket = case Clipboard.Board.create_topic() do
-      {:ok, topic} -> push_redirect(socket, to: Routes.show_topic_path(socket, :existing, topic.id))
-      {:error, _} -> raise "Could not created new topic"
-    end
+    socket =
+      case Clipboard.Board.create_topic() do
+        {:ok, topic} ->
+          push_redirect(socket, to: Routes.show_topic_path(socket, :existing, topic.id))
+
+        {:error, _} ->
+          raise "Could not created new topic"
+      end
 
     {:noreply, socket}
   end
